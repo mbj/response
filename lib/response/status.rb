568d4a53e775eb42769524b145b27ad35ed4341b
Markus Schirp <mbj@schirp-dso.com>
2013-03-21 14:58:25 +0100

    Add Response::Status::INTERNAL_SERVER_ERROR

--- lib/response/status.rb
+++ lib/response/status.rb
@@ -5,14 +5,16 @@ class Response
 
     private_class_method :new
 
-    OK                = new(200, 'OK'               )
-    CREATED           = new(201, 'Created'          )
-    NOT_FOUND         = new(404, 'Not Found'        )
-    NOT_MODIFIED      = new(304, 'Not Modified'     )
-    FOUND             = new(302, 'Found'            )
-    MOVED_PERMANENTLY = new(301, 'Moved Permanently')
-    NOT_AUTHORIZED    = new(401, 'Not Authorized'   )
-    FORBIDDEN         = new(403, 'Forbidden'        )
+    OK                    = new(200, 'OK'                   )
+    CREATED               = new(201, 'Created'              )
+    NOT_FOUND             = new(404, 'Not Found'            )
+    NOT_MODIFIED          = new(304, 'Not Modified'         )
+    FOUND                 = new(302, 'Found'                )
+    MOVED_PERMANENTLY     = new(301, 'Moved Permanently'    )
+    BAD_REQUEST           = new(400, 'Bad Request'          )
+    NOT_AUTHORIZED        = new(401, 'Not Authorized'       )
+    FORBIDDEN             = new(403, 'Forbidden'            )
+    INTERNAL_SERVER_ERROR = new(500, 'Internal Server Error')
 
   end
 end
